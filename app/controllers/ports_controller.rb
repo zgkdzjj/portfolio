c005c499fd0e21881e31368769d791943a230ea8
smegit <jesse.zha@swwcc.com.au>
2018-09-13 22:03:56 +1000

    Integreated new and create action to Portfolio

--- app/controllers/ports_controller.rb
+++ app/controllers/ports_controller.rb
@@ -2,4 +2,25 @@ class PortsController < ApplicationController
   def index
     @portfolio_items = Port.all  
   end
+
+  def new
+    @portfolio_item = Port.new
+  end
+
+  def create 
+    @portfolio_item = Port.new(params.require(:port).permit(:title, 
+      :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to ports_path, notice: 'Portfolio was successfully created.' }
+        format.json { render :show, status: :created, location: @portfolio_item }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
 end
