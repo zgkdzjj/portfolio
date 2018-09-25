44183440adf1f2c5530d7f2823f413f02d1e387a
smegit <jesse.zha@swwcc.com.au>
2018-09-25 22:26:02 +1000

    Added validation to blog skill portfolio

--- app/models/port.rb
+++ app/models/port.rb
@@ -1,2 +1,6 @@
 class Port < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :main_image, presence: true
+  validates :thumb_image, presence: true
 end
