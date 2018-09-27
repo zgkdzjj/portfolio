0d2c32776fa8641128a37c8d6685aef853a0ea9c
smegit <jesse.zha@swwcc.com.au>
2018-09-27 23:00:20 +1000

    Integrated concern for managing image data

--- app/models/port.rb
+++ app/models/port.rb
@@ -1,4 +1,5 @@
 class Port < ApplicationRecord
+  include Placeholder
   validates :title, :body, :main_image, :thumb_image, presence: true
   after_initialize :set_default
 
@@ -9,8 +10,8 @@ class Port < ApplicationRecord
   scope :ruby_on_rails_portfolio_items, -> {where(subtitle: 'Ruby on rails')}
 
   def set_default
-    self.main_image ||= 'https://via.placeholder.com/600*400'
-    self.thumb_image ||= 'https://via.placeholder.com/350x200'
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
 
 end
